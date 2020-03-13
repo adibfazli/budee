db90d9e5391496d62f2962e4115d29befd9b9063
Adib Fazli <adib.fazlii@gmail.com>
2020-03-13 14:40:43 -0700

    setting up the server, and fixing the server problems

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser')
 
 const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser())
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(session({ secret: 'zartzart' , resave: false , saveUninitialized: true }));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+  });
--- src/componants/gameBoard/GameBoard.jsx
+++ src/componants/gameBoard/GameBoard.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React , {useState} from 'react';
 import styles from './GameBoard.module.css';
 
 //this is the main board of the game, which contains 64 cell
 const Board = (props) =>{
     let table = []
-    for (let i=0 ; i<64 ; i++) table.push(<div></div>)
-    
+    //creating 64 cell for the game board
+    for (let i=0 ; i<64 ; i++) table.push(i)
+    const [cellStatus , SetCellStatus] = useState()
+
+    const divStyle = {
+        backgroundColor: 'gray',
+      };
     return (
+    
         <div className={styles.body}>
+            {console.log(table)}
             <div className={styles.Board_cell_container}>
-                {table}
+                {/* usig the table variable that contains 64 blank div */}
+                {table.map((e)=>{ 
+                    
+                    return ( <div 
+                        key={e} 
+                        className={styles.e} 
+                        value={e}
+                        onClick={(evt)=>{ 
+                            
+                        }}></div>)
+                })}
             </div>
         </div>
     )
